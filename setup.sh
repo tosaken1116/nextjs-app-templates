000e3a4083b472667438c2d03f3ddc64d493803f
tosaken1116 <nagato20021122@gmail.com>
2023-09-11 23:00:29 +0900

    fix:セットアップ時のコミットログの削除

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+rm -rf ./.git
+git init
 node ./setup.js
 rm -r node_modules
 rm -f package-lock.json pnpm-lock.yaml yarn.lock bun.lockb
 rm ./setup.js
 rm ./setup.sh
 git add .
-git commit --no-verify -m "setup"
+git commit --no-verify -m "setup with https://github.com/tosaken1116/nextjs-app-templates"
