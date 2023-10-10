7570c33d1cee437f72c423246543fbc95412a6e8
tosaken1116 <nagato20021122@gmail.com>
2023-10-10 10:59:00 +0900

    fix: page component generator error

--- _templates/generator/page/prompt.js
+++ _templates/generator/page/prompt.js
@@ -1,13 +1,12 @@
 const fs = require('fs');
 const path = require('path');
-const targetDirectory = "./src/components/model"
+const targetDirectory = './src/components/model';
 function getDirectories(basePath) {
-  return fs.readdirSync(basePath).filter(file => {
+  return fs.readdirSync(basePath).filter((file) => {
     const filePath = path.join(basePath, file);
     return fs.statSync(filePath).isDirectory();
   });
 }
-
 
 const directories = getDirectories(targetDirectory);
 
@@ -20,21 +19,22 @@ module.exports = {
           name: 'name',
           message: 'component name',
         },
-        {
+        directories.length > 0 && {
           type: 'multiselect',
           name: 'used_models',
           message: 'use model',
-          choices: directories
+          choices: directories,
         },
         {
           type: 'input',
           name: 'url',
           message: 'page_url localhost:3000/[] example:user/my',
         },
-      ]).then(({ name, used_models,url}) => ({
+      ])
+      .then(({ name, used_models, url }) => ({
         name,
-        used_models,
-        url
+        used_models: used_models ?? [],
+        url,
       }));
   },
 };
